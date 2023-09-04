b05939628ddf16ee6f6fb988609b680634664401
Eric Weatherington <ericweatherington@gmail.com>
2023-09-03 22:56:35 -0400

    added code and fruit.csv file

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,7 @@
 # add your code here
+
+import pandas
+
+fruit_sales = pandas.DataFrame([[35, 21], [41, 34]], columns = ['Apples', 'Bananas'], index = ['2017', '2018'])
+
+fruit_sales.to_csv('fruit.csv')
